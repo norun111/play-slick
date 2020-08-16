c26648acd6b319a16a6bb7eebba07e61f39b0433
tomoyaueno29 <u.t012807@gmail.com>
2020-08-16 19:49:05 +0900

    Update UserController.scala

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,21 +1,55 @@
 package controllers
 
+import java.util.UUID
+
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
-import scala.concurrent._
+import scalikejdbc._
 import javax.inject.Inject
+import models._
+import daos.User
 
+object UserController {
+  // フォームの値を格納するケースクラス
+  case class UserForm(id: String, name: String, email: String, password: String)
+
+  // formから送信されたデータ ⇔ ケースクラスの変換を行う
+  val userForm = Form(
+    mapping(
+      "id"        -> nonEmptyText,
+      "name"      -> nonEmptyText(minLength = 1, maxLength = 20),
+      "email"     -> email,
+      "password"  -> nonEmptyText(minLength = 8, maxLength = 60),
+    )(UserForm.apply)(UserForm.unapply)
+  )
+}
 
 class UserController @Inject()(components: MessagesControllerComponents)
   extends MessagesAbstractController(components) {
+
+  import UserController._
 
   def signUp = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.signUp())
   }
 
 
-  def create = TODO
+  def create = Action { implicit request =>
+    DB.localTx {
+      implicit session =>
+        userForm.bindFromRequest.fold(
+          error => {
+            BadRequest(views.html.signUp())
+          },
+          form => {
+            val uuid = UUID.randomUUID
+            User.create(uuid.toString, form.name, form.email, form.password)
+            Redirect(routes.HomeController.index())
+          }
+        )
+    }
+  }
 
 
   def edit(id: Option[String]) = TODO
