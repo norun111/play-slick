96488a41f794ebe74fbaf0f8963a8e1df5ae08e9
tomoyaueno29 <u.t012807@gmail.com>
2020-08-16 17:54:48 +0900

    Create SignUpForm.scala

---
+++ app/forms/SignUpForm.scala
@@ -0,0 +1,15 @@
+package forms
+
+//import models.services.CredentialsSingUpData
+//import play.api.data.Form
+//import play.api.data.Forms._
+//
+//object SignUpForm {
+//  val form = Form(
+//    mapping(
+//      "name" -> nonEmptyText,
+//      "email" -> email,
+//      "password" -> nonEmptyText,
+//    )(CredentialsSingUpData.apply)(CredentialsSingUpData.unapply)
+//  )
+//}
