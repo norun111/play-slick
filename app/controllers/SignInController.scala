03b91408bc6b1f2c49b6e0e52491c814c6745341
tomoyaueno29 <u.t012807@gmail.com>
2020-08-20 15:08:14 +0900

    Update SignInController.scala

--- app/controllers/SignInController.scala
+++ app/controllers/SignInController.scala
@@ -1,5 +1,40 @@
 package controllers
 
-class SignInController {
+import models._
+import utils.silhouette._
+import utils.silhouette.Implicits._
+import com.mohiva.play.silhouette.api.{ Silhouette, LoginInfo, SignUpEvent, LoginEvent, LogoutEvent }
+import com.mohiva.play.silhouette.api.repositories.AuthInfoRepository
+import com.mohiva.play.silhouette.api.util.{ Credentials, PasswordHasherRegistry, Clock }
+import com.mohiva.play.silhouette.api.exceptions.ProviderException
+import com.mohiva.play.silhouette.impl.providers.CredentialsProvider
+import com.mohiva.play.silhouette.impl.authenticators.CookieAuthenticator
+import com.mohiva.play.silhouette.impl.exceptions.{ IdentityNotFoundException, InvalidPasswordException }
+import com.mohiva.play.silhouette.api.Authenticator.Implicits._
+import play.api._
+import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
+import play.api.i18n.{ MessagesApi, Messages }
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits._
+import scala.concurrent.duration._
+import javax.inject.{ Inject, Singleton }
+import views.html.{ auth => viewsAuth }
+
+@Singleton
+class SignInController @Inject() (val silhouette: Silhouette[MyEnv],
+                                  val messagesApi: MessagesApi,
+                                  userService: UserService,
+                                  authInfoRepository: AuthInfoRepository,
+                                  credentialsProvider: CredentialsProvider,
+                                  tokenService: MailTokenService[MailTokenUser],
+                                  passwordHasherRegistry: PasswordHasherRegistry,
+                                  mailer: Mailer,
+                                  conf: Configuration,
+                                  clock: Clock
+                                 ) extends AuthController  {
+  {
 
 }
