7d0f18a28fc6833444d5d7a36de0b87e3905d926
tomoyaueno29 <u.t012807@gmail.com>
2020-08-20 15:08:19 +0900

    Update user.scala

--- app/models/user.scala
+++ app/models/user.scala
@@ -1,9 +1,15 @@
 package models
 
 import java.util.UUID
+import utils.silhouette.IdentitySilhouette
+import com.mohiva.play.silhouette.password.BCryptPasswordHasher
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 case class User(id: String = UUID.randomUUID.toString,
                 name: String,
                 email: String,
                 password: String
-               )
+               ) extends IdentitySilhouette {
+  def key = email
+}
