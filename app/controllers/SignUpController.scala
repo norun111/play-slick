607a6b416aebccfb7bf171f6dd81cb9dd9d5b44f
tomoyaueno29 <u.t012807@gmail.com>
2020-08-21 18:19:10 +0900

    Update SignUpController.scala

--- app/controllers/SignUpController.scala
+++ app/controllers/SignUpController.scala
@@ -1,52 +1,91 @@
 package controllers
 
 import java.util.UUID
+
+import com.mohiva.play.silhouette.api._
+import com.mohiva.play.silhouette.impl.providers._
+import forms.SignUpForm
 import javax.inject.Inject
-import models.daos.User
-import play.api.data.Forms._
-import play.api.data._
-import play.api.i18n._
-import play.api.mvc._
-import scalikejdbc._
+import models.User
+import play.api.i18n.Messages
+import play.api.libs.mailer.Email
+import play.api.mvc.{ AnyContent, Request }
+import utils.route.Calls
 
-object SignUpController {
-  // フォームの値を格納するケースクラス
-  case class UserForm(name: String, email: String, password: String)
+import scala.concurrent.{ ExecutionContext, Future }
 
-  // formから送信されたデータ ⇔ ケースクラスの変換を行う
-  val userForm = Form(
-    mapping(
-      "name"      -> nonEmptyText(minLength = 1, maxLength = 20),
-      "email"     -> email,
-      "password"  -> nonEmptyText(minLength = 8, maxLength = 60),
-    )(UserForm.apply)(UserForm.unapply)
-  )
-}
+/**
+ * The `Sign Up` controller.
+ */
+class SignUpController @Inject() (
+                                   components: SilhouetteControllerComponents,
+                                   signUp: views.html.signUp
+                                 )(implicit ex: ExecutionContext) extends SilhouetteController(components) {
 
-class SignUpController @Inject()(components: MessagesControllerComponents)
-  extends MessagesAbstractController(components) with I18nSupport {
-
-  import SignUpController._
-
-  def signUp = Action { implicit request =>
-    Ok(views.html.user.signUp.apply(userForm))
+  /**
+   * Views the `Sign Up` page.
+   *
+   * @return The result to display.
+   */
+  def view = UnsecuredAction.async { implicit request: Request[AnyContent] =>
+    Future.successful(Ok(signUp(SignUpForm.form)))
   }
 
+  /**
+   * Handles the submitted form.
+   *
+   * @return The result to display.
+   */
+  def submit = UnsecuredAction.async { implicit request: Request[AnyContent] =>
+    SignUpForm.form.bindFromRequest.fold(
+      form => Future.successful(BadRequest(signUp(form))),
+      data => {
+        val result = Redirect(routes.SignUpController.view()).flashing("info" -> Messages("sign.up.email.sent", data.email))
+        val loginInfo = LoginInfo(CredentialsProvider.ID, data.email)
+        userService.retrieve(loginInfo).flatMap {
+          case Some(user) =>
+            val url = Calls.signin.absoluteURL()
+            mailerClient.send(Email(
+              subject = Messages("email.already.signed.up.subject"),
+              from = Messages("email.from"),
+              to = Seq(data.email),
+              bodyText = Some(views.txt.emails.alreadySignedUp(user, url).body),
+              bodyHtml = Some(views.html.emails.alreadySignedUp(user, url).body)
+            ))
 
-  def create = Action { implicit request =>
-    DB.localTx {
-      implicit session =>
-        userForm.bindFromRequest.fold(
-          error => {
-            BadRequest(views.html.user.signUp.apply(error))
-          },
-          form => {
-            println("Hi")
-            val uuid = UUID.randomUUID
-            User.create(uuid.toString, form.name, form.email, form.password)
-            Redirect(routes.HomeController.index())
-          }
-        )
-    }
+            Future.successful(result)
+          case None =>
+            val authInfo = passwordHasherRegistry.current.hash(data.password)
+            val user = User(
+              userID = UUID.randomUUID(),
+              loginInfo = loginInfo,
+              firstName = Some(data.firstName),
+              lastName = Some(data.lastName),
+              fullName = Some(data.firstName + " " + data.lastName),
+              email = Some(data.email),
+              avatarURL = None,
+              activated = false
+            )
+            for {
+              avatar <- avatarService.retrieveURL(data.email)
+              user <- userService.save(user.copy(avatarURL = avatar))
+              authInfo <- authInfoRepository.add(loginInfo, authInfo)
+              authToken <- authTokenService.create(user.userID)
+            } yield {
+              val url = routes.ActivateAccountController.activate(authToken.id).absoluteURL()
+              mailerClient.send(Email(
+                subject = Messages("email.sign.up.subject"),
+                from = Messages("email.from"),
+                to = Seq(data.email),
+                bodyText = Some(views.txt.emails.signUp(user, url).body),
+                bodyHtml = Some(views.html.emails.signUp(user, url).body)
+              ))
+
+              eventBus.publish(SignUpEvent(user, request))
+              result
+            }
+        }
+      }
+    )
   }
 }
