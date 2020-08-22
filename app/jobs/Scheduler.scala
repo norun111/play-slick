5f83fa87a337f8bad434a74bd1ea45bddc3f8885
tomoyaueno29 <u.t012807@gmail.com>
2020-08-22 16:38:46 +0900

    Update Scheduler.scala

--- app/jobs/Scheduler.scala
+++ app/jobs/Scheduler.scala
@@ -8,11 +8,11 @@ import com.typesafe.akka.extension.quartz.QuartzSchedulerExtension
 /**
  * Schedules the jobs.
  */
-class Scheduler @Inject() (
-                            system: ActorSystem,
-                            @Named("auth-token-cleaner") authTokenCleaner: ActorRef) {
-
-  QuartzSchedulerExtension(system).schedule("AuthTokenCleaner", authTokenCleaner, AuthTokenCleaner.Clean)
-
-  authTokenCleaner ! AuthTokenCleaner.Clean
-}
+//class Scheduler @Inject() (
+//                            system: ActorSystem,
+//                            @Named("auth-token-cleaner") authTokenCleaner: ActorRef) {
+//
+//  QuartzSchedulerExtension(system).schedule("AuthTokenCleaner", authTokenCleaner, AuthTokenCleaner.Clean)
+//
+//  authTokenCleaner ! AuthTokenCleaner.Clean
+//}
