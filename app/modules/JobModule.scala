19efff2ee4c5b15999d0d8df44d4c7c85aca2c28
tomoyaueno29 <u.t012807@gmail.com>
2020-08-22 16:38:54 +0900

    Update JobModule.scala

--- app/modules/JobModule.scala
+++ app/modules/JobModule.scala
@@ -1,6 +1,6 @@
 package modules
 
-import jobs.{ AuthTokenCleaner, Scheduler }
+//import jobs.{ AuthTokenCleaner, Scheduler }
 import net.codingwell.scalaguice.ScalaModule
 import play.api.libs.concurrent.AkkaGuiceSupport
 
@@ -12,8 +12,8 @@ class JobModule extends ScalaModule with AkkaGuiceSupport {
   /**
    * Configures the module.
    */
-  override def configure() = {
-    bindActor[AuthTokenCleaner]("auth-token-cleaner")
-    bind[Scheduler].asEagerSingleton()
-  }
+  //  override def configure() = {
+  //    bindActor[AuthTokenCleaner]("auth-token-cleaner")
+  //    bind[Scheduler].asEagerSingleton()
+  //  }
 }
